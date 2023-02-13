acf33652f64109e9105c16bd39e4946fc93fc0ef
xiangying <1984997880@qq.com>
2023-02-13 20:23:48 +0800

    checkstyle

--- pulsar/transactionCoordinatorClient.go
+++ pulsar/transactionCoordinatorClient.go
@@ -1,14 +1,32 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package pulsar
 
 import (
 	"context"
+	"strconv"
+	"sync/atomic"
+	"time"
+
 	"github.com/apache/pulsar-client-go/pulsar/internal"
 	pb "github.com/apache/pulsar-client-go/pulsar/internal/pulsar_proto"
 	"github.com/apache/pulsar-client-go/pulsar/log"
 	"google.golang.org/protobuf/proto"
-	"strconv"
-	"sync/atomic"
-	"time"
 )
 
 type transactionCoordinatorClient struct {
@@ -100,19 +118,19 @@ func (tc *transactionCoordinatorClient) newTransaction(timeout time.Duration) (T
 		return TxnID{}, err
 	}
 	requestID := tc.client.rpcClient.NewRequestID()
-	nextTcId := tc.nextTCNumber()
+	nextTcID := tc.nextTCNumber()
 	cmdNewTxn := &pb.CommandNewTxn{
 		RequestId:     proto.Uint64(requestID),
-		TcId:          proto.Uint64(nextTcId),
+		TcId:          proto.Uint64(nextTcID),
 		TxnTtlSeconds: proto.Uint64(uint64(timeout.Milliseconds())),
 	}
 
-	cnx, err := tc.client.rpcClient.RequestOnCnx(tc.cons[nextTcId], requestID, pb.BaseCommand_NEW_TXN, cmdNewTxn)
+	cnx, err := tc.client.rpcClient.RequestOnCnx(tc.cons[nextTcID], requestID, pb.BaseCommand_NEW_TXN, cmdNewTxn)
 	if err != nil {
 		return TxnID{}, err
 	}
 
-	return TxnID{nextTcId, *cnx.Response.NewTxnResponse.TxnidLeastBits}, nil
+	return TxnID{nextTcID, *cnx.Response.NewTxnResponse.TxnidLeastBits}, nil
 }
 
 /*
